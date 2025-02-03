b74989a5ceeee64f32994f786a4a596325651382
ingfosbreak <ingfosbreak@outlook.com>
2025-02-03 16:09:55 +0700

    update: model.py (not finished)

--- api/models.py
+++ api/models.py
@@ -6,6 +6,32 @@ class User(AbstractUser):
     ROLE_CHOICES = (
         ('staff', 'Staff'),
         ('user', 'User'),
+        ('teacher', 'Teacher')
     )
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="students")
+    name = models.CharField(max_length=100)
+    dob  = models.DateField()
+    contact = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    pass
+
+class Level(models.Model):
+    levelName = models.CharField(max_length=50)
+
+class Course(models.Model):
+    courseName = models.CharField(max_length=100)
+    description = models.TextField()
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name="courses")
+
+class CourseSession(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="sessions")
+    # teacher = 
