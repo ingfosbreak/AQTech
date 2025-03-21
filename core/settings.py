fd4aad723e8f3213ed109c62a04e0677d860675f
ingfosbreak <ingfosbreak@outlook.com>
2025-03-21 14:24:35 +0700

    fix: read.env

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ import environ
 import stripe
 
 env = environ.Env()
-environ.Env.read_env(".env")
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
