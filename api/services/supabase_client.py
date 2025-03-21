3fe2dca446f1c99576d8f6e80658beac95fe31fd
ingfosbreak <ingfosbreak@outlook.com>
2025-03-21 15:08:54 +0700

    fix: loadend

--- api/services/supabase_client.py
+++ api/services/supabase_client.py
@@ -1,8 +1,8 @@
 from supabase import create_client
-# from django.conf import settings
+from django.conf import settings
 
 # Initialize Supabase client
-supabase = create_client("https://sryesoktrkolkclrolcf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyeWVzb2t0cmtvbGtjbHJvbGNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTcyOTkyNCwiZXhwIjoyMDU3MzA1OTI0fQ.zurJ0zO9A5m-Y-MmZkvgMOls7TVN2KUWLgwotnfUh18")
+supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
 def get_supabase_client():
     return supabase
