edb85e6f98914761393fafb7b93aca7fd65c72bb
ingfosbreak <ingfosbreak@outlook.com>
2025-03-21 15:18:56 +0700

    fix: docker migrate

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.abspath(__file__)))
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 
